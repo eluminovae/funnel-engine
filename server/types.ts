5f109ba691e0858000392353a3b4ed52891f777a
loonamoor <melody.lily.13@gmail.com>
2026-05-11 01:36:36 +0300

    Set up server in index.ts, prepared types for MongoDB collections in types.ts

---
+++ server/types.ts
@@ -0,0 +1,30 @@
+// КОЛЛЕКЦИИ В MONGODB
+export interface User { // Примеры:
+  telegramID: number; // 74724...
+  userName?: string; // @username
+  firstName: string; // John Doe
+  enrolledCourses: string[]; // ['en', 'de']
+  currentStep: string; // 'wait_video' | 'test_audio' и т.д.
+  selectedLanguage: string; // 'de'
+}
+
+export interface Course {
+  courseCode: string; // 'en'
+  title: string; // 'English Premium'
+  description: string; // 'Это самый лучший курс для изучения англ языка'
+  videoFileId: string; // file.id (?) медиа в серверах Телеграм
+  isActive: boolean; // true (чтобы быстро можно было скрыть курс)
+}
+
+export interface Button {
+  text: string; // 'Забрать подарок'
+  callbackData?: string // msg_choose_level
+  managerUserName?: string // вдруг понадобится связь с человеком
+  url?: string
+}
+
+export interface BotCommandMessage {
+  key: string; // msg_welcome
+  text: string // 'Привет! Твой подарок - PDF «Топ-100 фраз для выживания за границей»'
+  buttons?: Button[][]; // ['Забрать подарок']
+}
